2dad84f1c17664d19031b7b7eeb2d7828f95651d
Patrick Neschkudla <neschkudla@gmail.com>
2017-11-02 16:33:05 +0100

    Create asyncTheme.js

---
+++ asyncTheme.js
@@ -0,0 +1,21 @@
+{
+  colors: {
+    primary: '#000000',
+  },
+  logo: {
+    style: {
+      backgroundImage: `url("https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png")`
+    }
+  },
+  'auth.layout': {
+    style: {
+      backgroundImage: `url("https://www.toptal.com/designers/subtlepatterns/patterns/spiration-dark.png")`,
+      backgroundRepeat: 'repeat',
+    }
+  },
+  'auth.gradient': {
+    style: {
+      background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.8) 0%, rgba(255,255,255,0) 100%)'
+    }
+  }
+};
