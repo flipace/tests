a35c5a07e3ac6178f2159892741cceb2abda0f7f
Patrick Neschkudla <neschkudla@gmail.com>
2017-11-02 17:10:39 +0100

    Update asyncTheme.js

--- asyncTheme.js
+++ asyncTheme.js
@@ -4,7 +4,7 @@
   },
   "logo": {
     "style": {
-      "backgroundImage": "url('http://imageog.flaticon.com/icons/png/512/3/3641.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF')",
+      "backgroundImage": "url('http://imageog.flaticon.com/icons/png/512/3/3641.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=transparent')",
       "width": "300px",
       "height": "300px",
       "backgroundSize": "cover"
